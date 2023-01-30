72df30f608a6b4ef5399fa4bbd8fe8f9580d665e
Nickname <55950404+Nicknaem@users.noreply.github.com>
2023-01-30 04:23:53 +0400

    sw update

--- sw.js
+++ sw.js
@@ -1,19 +1,56 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('fox-store').then((cache) => cache.addAll([
-        '/Niksapp/',
-        '/Niksapp/index.html',
-        '/Niksapp/main.js',
-        '/Niksapp/main.css',
-        '/Niksapp/assets/backgrounds/1.jpg',
-        '/Niksapp/assets/logos/n.png',
+const cacheName = "cache1"; // Change value to force update
+
+self.addEventListener('install', event => {
+    // Kick out the old service worker
+	self.skipWaiting();
+
+    event.waitUntil(
+      caches.open(cacheName).then(cache => cache.addAll([
+        '/',
+        'index.html',
+        'main.js',
+        'main.css',
+        'assets/backgrounds/1.jpg',
+        'assets/logos/n.png',
       ])),
     );
   });
-  
-  self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
-  });
+
+  self.addEventListener("activate", event => {
+	// Delete any non-current cache
+	event.waitUntil(
+		caches.keys().then(keys => {
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		})
+	);
+});
+
+// Offline-first, cache-first strategy
+// Kick off two asynchronous requests, one to the cache and one to the network
+// If there's a cached version available, use it, but fetch an update for next time.
+// Gets data on screen as quickly as possible, then updates once the network has returned the latest data. 
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
+});
+
+//   self.addEventListener('fetch', (e) => {
+//     console.log(e.request.url);
+//     e.respondWith(
+//       caches.match(e.request).then((response) => response || fetch(e.request)),
+//     );
+//   });
